74667df8da6caa0d140ca1dd9d83fc1e210f4bd9
uet20020715vnu <quangthai3110@gmail.com>
2024-05-30 09:09:43 +0700

    category

---
+++ src/app/services/Category.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ApiResponse } from '../model/ApiResponse';
+import { PaginationResponse } from '../model/Pagination';
+import { Category } from '../model/Category';
+const url = environment.endPoint;
+const endPoint = 'category';
+const params = '?page=1&limit=10';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+  constructor(private http: HttpClient) {}
+
+  getCategory(): Observable<any> {
+    return this.http.get<Category[]>(url + endPoint +'/', {});
+  }
+  
+  getCategorys(searchParam: any): Observable<PaginationResponse<Category[]>> {
+    return this.http.post<PaginationResponse<Category[]>>(
+      `${url + endPoint + '/search'}`,
+      searchParam
+    );
+  }
+
+  // getById(Id: number): Observable<ApiResponse<Category>> {
+  //   return this.http.get<ApiResponse<Category>>(`${url + endPoint + '/' + Id}`);
+  // }
+
+  getById(Id: number): Observable<any> {
+    return this.http.get<Category>(`${url + endPoint + '/' + Id}`);
+  }
+
+  createCategory(category: Category): Observable<Category> {
+    return this.http.post<Category>(`${url + endPoint}/add`, category);
+  }
+
+  updateCategory(category: Category): Observable<ApiResponse<Category>> {
+    return this.http.put<ApiResponse<Category>>(
+      `${url + endPoint + '/' + category.id}`,
+      category
+    );
+  }
+
+  deleteCategory(Id: number): Observable<ApiResponse<boolean>> {
+    return this.http.delete<ApiResponse<boolean>>(
+      `${url + endPoint + '/' + Id}`
+    );
+  }
+}
