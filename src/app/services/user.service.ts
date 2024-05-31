44775a002de7239be6c4856681ed8f49a9a27751
PhuTam54 <phutamytb@gmail.com>
2024-05-31 15:49:33 +0700

    Add paginate for user

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -7,7 +7,6 @@ import { PaginationResponse } from '../model/Pagination';
 import { User } from '../model/User';
 const url = environment.endPoint;
 const endPoint = 'users';
-const defaultParams = '?page=1&limit=10';
 @Injectable({
   providedIn: 'root',
 })
@@ -15,8 +14,8 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   getUser(searchParam: any): Observable<any> {
-    // return this.http.get<User[]>(url + endPoint + searchParam ?? defaultParams, {});
-    return this.http.get<User[]>(url + endPoint + defaultParams, {});
+    return this.http.get<User[]>(url + endPoint + '?page=' 
+    + searchParam.pageNumber + '&limit=' + searchParam.pageSize, {});
   }
   
   getUsers(searchParam: any): Observable<PaginationResponse<User[]>> {
