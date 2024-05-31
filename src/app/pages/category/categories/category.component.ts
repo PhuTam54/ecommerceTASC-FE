55ea846a5e3ab501016746b9c5e75ab478a864ab
uet20020715vnu <quangthai3110@gmail.com>
2024-05-31 23:15:06 +0700

    pageable category

--- src/app/pages/category/categories/category.component.ts
+++ src/app/pages/category/categories/category.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PaginationResponse } from 'src/app/model/Pagination';
 import { Category } from '../../../model/Category';
 import { CategoryService } from '../../../services/category.service';
@@ -10,19 +10,30 @@ import { CategoryService } from '../../../services/category.service';
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
-  constructor(private categoryService: CategoryService, private router: Router) {}
-
+  constructor(private categoryService: CategoryService, private router: Router,private route: ActivatedRoute) {}
+  paginationRes = new PaginationResponse<Category[]>();
+  searchParam = {pageNumber: 1, pageSize: 10};
 
   category: Category[] = [];
+  totalPages: number = 0;
+  pages: number[] | undefined;
 
   ngOnInit() {
+    console.log(this.searchParam)
     this.getCategory();
   }
 
-  getCategory() {
-    this.categoryService.getCategory().subscribe((res) => {
-      console.log(res);
-      this.category = res;
+  getCategory(searchParam: any = this.searchParam) {
+    if (this.route.snapshot.queryParamMap.get('page')) {
+      searchParam.pageNumber = this.route.snapshot.queryParamMap.get('page');
+    }
+    if (this.route.snapshot.queryParamMap.get('limit')) {
+      searchParam.pageSize = this.route.snapshot.queryParamMap.get('limit');
+    }
+    this.categoryService.getCategory(searchParam).subscribe((res) => {
+      this.category = res.content;
+      this.totalPages = res.totalPages;
+      this.pages = Array.from({length: this.totalPages}, (_, i) => i + 1);
     });
   }
 
@@ -42,5 +53,29 @@ export class CategoryComponent implements OnInit {
         });
   
   }
+  prevPage() {
+    if (this.searchParam['pageNumber'] > 1) {
+      this.searchParam['pageNumber']--;
+      this.router.navigateByUrl('/category?page=' + this.searchParam['pageNumber'] + '&limit=' + this.searchParam['pageSize'])
+      .then(() => {
+        this.getCategory(this.searchParam);
+      });
+    }
+  }
 
+  nextPage() {
+    if (this.searchParam['pageNumber'] < this.totalPages) {
+      this.searchParam['pageNumber']++;
+      this.router.navigateByUrl('/category?page=' + this.searchParam['pageNumber'] + '&limit=' + this.searchParam['pageSize'])
+      .then(() => {
+        this.getCategory(this.searchParam);
+      });
+    }
+  }
+  searchParamChange(page: number) {
+    this.router.navigate(['/category'], { queryParams: { page: page, limit: this.searchParam['pageSize'] } })
+    .then(() => {
+      this.getCategory(this.searchParam);
+    });
+  }
 }
